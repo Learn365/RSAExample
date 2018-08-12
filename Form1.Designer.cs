ccc9bc016806093dc02dc4593b1962bd7dc6964a
baikangwang <baikangwang@hotmail.com>
2018-08-12 22:10:30 +0800

    initial commit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,141 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.txtPlain = new System.Windows.Forms.TextBox();
+            this.txtEncrypted = new System.Windows.Forms.TextBox();
+            this.txtExpected = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Arial", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(799, 53);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Asymmetric Encryption-RSA";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnEncrypt);
+            this.groupBox1.Controls.Add(this.txtPlain);
+            this.groupBox1.Location = new System.Drawing.Point(13, 77);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(799, 117);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Plain Text For Encryption";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.txtEncrypted);
+            this.groupBox2.Location = new System.Drawing.Point(13, 214);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(799, 123);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Encrypted Text";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtExpected);
+            this.groupBox3.Location = new System.Drawing.Point(12, 354);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(799, 118);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Plain Text After Decryption";
+            // 
+            // txtPlain
+            // 
+            this.txtPlain.Location = new System.Drawing.Point(6, 20);
+            this.txtPlain.Multiline = true;
+            this.txtPlain.Name = "txtPlain";
+            this.txtPlain.Size = new System.Drawing.Size(706, 81);
+            this.txtPlain.TabIndex = 0;
+            // 
+            // txtEncrypted
+            // 
+            this.txtEncrypted.Location = new System.Drawing.Point(6, 20);
+            this.txtEncrypted.Multiline = true;
+            this.txtEncrypted.Name = "txtEncrypted";
+            this.txtEncrypted.Size = new System.Drawing.Size(706, 84);
+            this.txtEncrypted.TabIndex = 0;
+            // 
+            // txtExpected
+            // 
+            this.txtExpected.Location = new System.Drawing.Point(8, 20);
+            this.txtExpected.Multiline = true;
+            this.txtExpected.Name = "txtExpected";
+            this.txtExpected.Size = new System.Drawing.Size(785, 85);
+            this.txtExpected.TabIndex = 0;
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(718, 20);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 81);
+            this.btnEncrypt.TabIndex = 1;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(718, 20);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 84);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(824, 484);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(840, 523);
+            this.MinimumSize = new System.Drawing.Size(840, 523);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.TextBox txtPlain;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtEncrypted;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtExpected;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,66 @@ namespace RSAExample
 {
     public partial class Form1 : Form
     {
+        private UnicodeEncoding _byteConvert = new UnicodeEncoding();
+        private RSACryptoServiceProvider _rsa = new RSACryptoServiceProvider();
+        private byte[] _plainText;
+        private byte[] _encryptedText;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public byte[] Encrypt(byte[] data, RSAParameters rsaKey, bool doOAEPPadding)
+        {
+            try
+            {
+                byte[] encryptedData;
+                using(RSACryptoServiceProvider rsa=new RSACryptoServiceProvider())
+                {
+                    rsa.ImportParameters(rsaKey);
+                    encryptedData = rsa.Encrypt(data, doOAEPPadding);
+                }
+                return encryptedData;
+            }
+            catch(CryptographicException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        public byte[] Decrypt(byte[] data, RSAParameters rsaKey, bool doOAEPPading)
+        {
+            try
+            {
+                byte[] decryptedData;
+                using (RSACryptoServiceProvider rsa=new RSACryptoServiceProvider())
+                {
+                    rsa.ImportParameters(rsaKey);
+                    decryptedData = rsa.Decrypt(data, doOAEPPading);
+                }
+
+                return decryptedData;
+            }
+            catch (CryptographicException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            this._plainText = _byteConvert.GetBytes(txtPlain.Text);
+            this._encryptedText = this.Encrypt(this._plainText, this._rsa.ExportParameters(false), false);
+            txtEncrypted.Text = Convert.ToBase64String(this._encryptedText);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            byte[] decryptedText = Decrypt(this._encryptedText, this._rsa.ExportParameters(true), false);
+            txtExpected.Text =  this._byteConvert.GetString(decryptedText);
         }
     }
 }
